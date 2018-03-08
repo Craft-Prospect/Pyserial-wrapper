1a09acaa08a26a20d350b53ae75c4421fc6f52e0
Murray Ireland <murray.ireland@craftprospect.com>
2018-03-08 08:42:18 +0000

    Added client_blank

---
+++ client_blank.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""client_blank.py: Client script for serial read/write"""
+
+import time
+from serial_wrapper import Serial
+from numpy import sin, cos
+
+# Start client timer
+TIME_INIT = time.time()
+
+# Create serial port object
+ser = Serial("/dev/ttyPS0")
+
+zero_time = ser.listen_for_handshake()
+
+# Close serial port  
+ser.close()
