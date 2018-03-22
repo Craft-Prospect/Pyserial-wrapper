8e8c78faebeb668afb9ff6b2df33a991d6a49800
Murray Ireland <murray.ireland@craftprospect.com>
2018-03-22 14:51:53 +0000

    Host working

--- host_bytearray.py
+++ host_bytearray.py
@@ -26,7 +26,7 @@ ser.write_terminal_cmd(cmd)
 # Send handshake
 handshake, zero_time = ser.send_handshake()
 
-# time.sleep(0.5)
+time.sleep(0.5)
 
 # Load image
 image_path = "test-small.jpg"
@@ -34,18 +34,23 @@ img = cv2.imread(image_path)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_dims = img.shape
 
+# fig = plt.figure()
+# axs = fig.subplots(1, 1)
+# axs.imshow(img)
+# plt.show()
+
 # # Send to client
 # ser.writeline("ready")
 
 # Wait for data signal
-loop = True
-while loop:
-    print("Waiting for data transmission")
-    time.sleep(0.5)
-    rcvd = ser.readline(timeout=0.02)
-    if rcvd != None and "ready" in rcvd:
-        loop = False
-        print("Data ready to read")
+# loop = True
+# while loop:
+#     print("Waiting for data transmission")
+#     time.sleep(0.5)
+#     rcvd = ser.readline(timeout=0.02)
+#     if rcvd != None and "ready" in rcvd:
+#         loop = False
+#         print("Data ready to read")
 
 # Receive altered image from client
 progress = tqdm(total=img_dims[0]*img_dims[1]*img_dims[2])
@@ -53,6 +58,7 @@ total_bytes = 0
 rcvd_acc = bytearray()
 while total_bytes < img_dims[0]*img_dims[1]*img_dims[2]:
     num_bytes = ser.ser.in_waiting
+    # print("Num bytes: {}".format(num_bytes))
     total_bytes += num_bytes
     rcvd = ser.ser.read(num_bytes)
     rcvd_acc.extend(rcvd)
