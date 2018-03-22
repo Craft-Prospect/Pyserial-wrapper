f4233f084606bb5446b41f81ab088c14c02eb76e
Murray Ireland <murray.ireland@craftprospect.com>
2018-03-22 13:48:56 +0000

    Added test images

--- host_bytearray.py
+++ host_bytearray.py
@@ -26,18 +26,26 @@ ser.write_terminal_cmd(cmd)
 # Send handshake
 handshake, zero_time = ser.send_handshake()
 
-time.sleep(0.5)
+# time.sleep(0.5)
 
 # Load image
-image_path = "/home/prospero/OBDA/simulation-test-bench/images/sample1/cam_imagery/time14.00s.jpg"
+image_path = "test-small.jpg"
 img = cv2.imread(image_path)
-# print(img)
-# print(type(img))
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_dims = img.shape
 
-# Send to client
+# # Send to client
+# ser.writeline("ready")
 
+# Wait for data signal
+loop = True
+while loop:
+    print("Waiting for data transmission")
+    time.sleep(0.5)
+    rcvd = ser.readline(timeout=0.02)
+    if rcvd != None and "ready" in rcvd:
+        loop = False
+        print("Data ready to read")
 
 # Receive altered image from client
 progress = tqdm(total=img_dims[0]*img_dims[1]*img_dims[2])
@@ -46,12 +54,9 @@ rcvd_acc = bytearray()
 while total_bytes < img_dims[0]*img_dims[1]*img_dims[2]:
     num_bytes = ser.ser.in_waiting
     total_bytes += num_bytes
-    # print("Num bytes: {}".format(num_bytes))
-    # print("Total bytes: {}".format(total_bytes))
     rcvd = ser.ser.read(num_bytes)
     rcvd_acc.extend(rcvd)
     progress.update(num_bytes)
-    # time.sleep(0.01)
 
 data = np.array(list(rcvd_acc))
 print("Number of bytes in data package: {}".format(len(data)))
@@ -62,7 +67,10 @@ data = np.reshape(data, img_dims)
 if (data == img).all():
     print("Images are identical")
 
-plt.imshow(data)
+fig = plt.figure()
+axs = fig.subplots(2, 1)
+axs[0].imshow(img)
+axs[1].imshow(data)
 plt.show()
 
 # Close serial port
